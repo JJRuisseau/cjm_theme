3608c688400b8bd81ab1a8a54d7c5ddb73c32b3e
JJ Ruisseau <alternate.utopia@gmail.com>
2021-11-02 15:47:29 +0100

    matomo

--- front-page.php
+++ front-page.php
@@ -4,6 +4,12 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <!-- Matomo --><script>
+    var _paq = window._paq = window._paq || [];
+    _paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);_paq.push(['alwaysUseSendBeacon']);_paq.push(['setTrackerUrl', "\/\/cjm.s-entraider.net\/wp-content\/plugins\/matomo\/app\/matomo.php"]);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src="\/\/cjm.s-entraider.net\/wp-content\/uploads\/matomo\/matomo.js"; s.parentNode.insertBefore(g,s);
+    </script>
+    <!-- End Matomo Code -->
 </head>
 <body>
     <header class="flex justify-center">
