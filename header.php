4d9e4544c97df0ec853ada610b58a7ab08ffde0a
JJ Ruisseau <alternate.utopia@gmail.com>
2021-11-04 20:37:54 +0100

    Matomo update

--- header.php
+++ header.php
@@ -8,11 +8,5 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <?php wp_head(); ?>
-    <!-- Matomo --><script>
-    var _paq = window._paq = window._paq || [];
-    _paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);_paq.push(['alwaysUseSendBeacon']);_paq.push(['setTrackerUrl', "\/\/cjm.s-entraider.net\/wp-content\/plugins\/matomo\/app\/matomo.php"]);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src="\/\/cjm.s-entraider.net\/wp-content\/uploads\/matomo\/matomo.js"; s.parentNode.insertBefore(g,s);
-    </script>
-    <!-- End Matomo Code -->
 </head>
 <body <?php body_class(); ?> >
